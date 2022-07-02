113fd07bbf87f50c76fa68f353e6832d599c2c47
adnan-geek <40834121+adnan-geek@users.noreply.github.com>
2022-07-02 21:17:48 +0100

    newmap

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const btn =  document.querySelector('.toggle-menu');
 const menu = document.querySelector('nav');
-
+const links = document.querySelectorAll('.nav-links a');
 btn.addEventListener("click", showMenu);
 
  function showMenu(){
 
     menu.classList.toggle('show');
 }
+
+links.forEach( (link)=>{
+    link.addEventListener("click",()=>{
+        menu.classList.remove('show');
+    })
+})
