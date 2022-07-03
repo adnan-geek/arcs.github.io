039cd9e47c1a6131f0379f85eb824864c21db4e6
adnan-geek <40834121+adnan-geek@users.noreply.github.com>
2022-07-03 17:30:24 +0100

    add-pic

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btn =  document.querySelector('.toggle-menu');
 const menu = document.querySelector('nav');
 const links = document.querySelectorAll('.nav-links a');
+const scrollTopBtn = document.querySelector('#scroll-top')
 btn.addEventListener("click", showMenu);
 
  function showMenu(){
@@ -12,4 +13,15 @@ links.forEach( (link)=>{
     link.addEventListener("click",()=>{
         menu.classList.remove('show');
     })
+})
+window.onscroll =  ()=>{
+   if (scrollY > 260) {
+    scrollTopBtn.style.display = 'block';
+   }
+   else {
+    scrollTopBtn.style.display = 'none';
+   }
+}
+scrollTopBtn.addEventListener('click', ()=>{
+        scrollTo(0,0);
 })
